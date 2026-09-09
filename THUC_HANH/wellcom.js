c6a50151d421960e56af4c57293b50ae785bcb20
Ho Thi Vai <thivai642@gmail.com>
2026-09-10 00:58:18 +0700

    update

---
+++ THUC_HANH/wellcom.js
@@ -0,0 +1,9 @@
+
+        // alert("Wellcom to my Website"); //xuất ra trang web thông báo
+        console.log("Wellcom to website");
+let sum = 0;
+let a = 10;
+let b = 20;
+sum = a + b;
+console.log("the sum of a and b:" +sum);
+
